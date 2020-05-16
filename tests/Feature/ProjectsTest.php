aa66880fcf2f3cde7df4504b9c86f11dc7a4029d
danish ikhwan <danishikhwanneymar@gmail.com>
2020-05-16 09:01:36 +0800

    first commit

---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProjectsTest extends TestCase
+{
+    use RefreshDatabase;
+
+ public function test_a_user_can_create_a_projects()
+ {
+
+ }
+}
